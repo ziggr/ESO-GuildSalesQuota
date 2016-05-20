b2472fb0859a60139b068ca60bd6d1e7fe9ebcb4
Zig Zichterman <ziggr@ziggr.com>
2016-05-20 00:20:35 -0700

    CSV conversion works

--- GuildSalesQuota_to_csv.lua
+++ GuildSalesQuota_to_csv.lua
@@ -1,36 +1,83 @@
--- Read the SavedVariables file that GuildGoldDeposits creates  and convert
+-- Read the SavedVariables file that GuildSalesQuota creates  and convert
 -- that to a spreadsheet-compabitle CSV (comma-separated value) file.
 
-IN_FILE_PATH  = "../../SavedVariables/GuildGoldDeposits.lua"
-OUT_FILE_PATH = "../../SavedVariables/GuildGoldDeposits.csv"
+IN_FILE_PATH  = "../../SavedVariables/GuildSalesQuota.lua"
+OUT_FILE_PATH = "../../SavedVariables/GuildSalesQuota.csv"
 dofile(IN_FILE_PATH)
 OUT_FILE = assert(io.open(OUT_FILE_PATH, "w"))
 
--- Lua lacks a split() function. Here's a cheesy hardwired one that works
+-- Lua lacks a split() function. Here's a cheesy one that works
 -- for our specific need.
-function split(str)
-    t1 = string.find(str, '\t')
-    t2 = string.find(str, '\t', 1 + t1)
-    return   string.sub(str, 1,      t1 - 1)
-           , string.sub(str, 1 + t1, t2 - 1)
-           , string.sub(str, 1 + t2)
+function split(str, delim)
+    local l = {}
+    local delim_index = 0
+    while true do
+        end_index = string.find(str, delim, delim_index + 1)
+        if end_index == nil then
+            local word = string.sub(str, delim_index + 1)
+            table.insert(l, word)
+            break
+        end
+        local word = string.sub(str, delim_index + 1, end_index - 1)
+        table.insert(l, word)
+        delim_index = end_index
+    end
+    return l
 end
 
--- Parse the ["history'] table
-function TableHistory(history)
-                        -- Sort by guild name to avoid Lua table's tendency
-                        -- to flip keys around randomly. Keep the file stable
-                        -- for diffing and human sanity.
-    for _, guild_name in pairs(sorted_keys(history)) do
-        v = history[guild_name]
-                        -- reverse alpha sort ISO 8601 dates will correctly
-                        -- put most recent history to top and oldest history
-                        -- to bottom of file.
-        table.sort(v, function(a,b) return b < a end )
-        for _,line in pairs(v) do
-            time_secs, amount, user = split(line)
-            WriteLine(guild_name, time_secs, amount, user)
+-- Sort by sold, descending. If that matches, sort by bought.
+function RecordCompare(a,b)
+    -- Group members before outlanders.
+    if b.is_member ~= a.is_member then return a.is_member end
+
+    -- First sort by amount sold, descending
+    if b.sold < a.sold then return true  end
+    if b.sold > a.sold then return false end
+
+    -- Then by amount bought, descending (big buyers are our friends!)
+    if b.bought < a.bought then return true  end
+    if b.bought > a.bought then return false end
+
+    -- And finally, by user id when there's nothing else meaningful.
+    return string.lower(b.user_id) > string.lower(a.user_id)
+end
+
+function WriteGuild(guild_name, guild_index, user_records)
+    OUT_FILE:write( "# guild"
+                     .. ",user_id"
+                     .. ",sold"
+                     .. ",bought"
+                     .. ",is_member"
+                     .. "\n" )
+
+    local records = {}
+
+                        -- Extract flat rows for this guild's records
+    for _, str in ipairs(user_records) do
+        local w = split(str, "\t")
+        local user_id = w[1]
+        local guild_str = w[1 + guild_index]
+        if guild_str and guild_str ~= "" then
+            ww = split(guild_str, " ")
+            local is_member = ww[1] == "true"
+            local bought    = tonumber(ww[2])
+            local sold      = tonumber(ww[3])
+
+            local row = { user_id = user_id
+                        , is_member = is_member
+                        , bought    = bought
+                        , sold      = sold
+                        }
+            table.insert(records, row)
         end
+    end
+
+                        -- Sort by sold amount, descending.
+    table.sort(records, RecordCompare)
+
+                        -- Dump to CSV
+    for _,row in ipairs(records) do
+        WriteLine(guild_name, row.user_id, row.sold, row.bought, row.is_member)
     end
 end
 
@@ -48,36 +95,30 @@ function enquote(s)
     return '"' .. s .. '"'
 end
 
--- Convert "1456709816" to "2016-02-28T17:36:56" ISO 8601 formatted time
--- Assume "local machine time" and ignore any incorrect offsets due to
--- Daylight Saving Time transitions. Ugh.
-function iso_date(secs_since_1970)
-    t = os.date("*t", secs_since_1970)
-    return string.format("%04d-%02d-%02dT%02d:%02d:%02d"
-                        , t.year
-                        , t.month
-                        , t.day
-                        , t.hour
-                        , t.min
-                        , t.sec
-                        )
-end
-
-function WriteLine(guild_name, time_secs, amount, user)
+function WriteLine(guild_name, user_id, sold, bought, is_member)
     OUT_FILE:write(          enquote(guild_name)
-          .. ',' .. iso_date(time_secs)
-          .. ',' .. amount
-          .. ',' .. enquote(user)
+          .. ',' .. enquote(user_id)
+          .. ',' .. sold
+          .. ',' .. bought
+          .. ',' .. tostring(is_member)
           .. '\n'
           )
 end
 
 
 -- For each account
-for k, v in pairs(GuildGoldDepositsVars["Default"]) do
-    if (    GuildGoldDepositsVars["Default"][k]["$AccountWide"]
-        and GuildGoldDepositsVars["Default"][k]["$AccountWide"]["history"]) then
-        TableHistory(GuildGoldDepositsVars["Default"][k]["$AccountWide"]["history"])
+for k, v in pairs(GuildSalesQuotaVars["Default"]) do
+    if (    GuildSalesQuotaVars["Default"][k]["$AccountWide"]
+        and GuildSalesQuotaVars["Default"][k]["$AccountWide"]["user_records"]) then
+        enable_guild = GuildSalesQuotaVars["Default"][k]["$AccountWide"]["enable_guild"]
+        guild_name   = GuildSalesQuotaVars["Default"][k]["$AccountWide"]["guild_name"  ]
+        user_records = GuildSalesQuotaVars["Default"][k]["$AccountWide"]["user_records"]
+
+        for guild_index, enabled in ipairs(enable_guild) do
+            if enabled then
+                WriteGuild(guild_name[guild_index], guild_index, user_records)
+            end
+        end
     end
 end
 OUT_FILE:close()
