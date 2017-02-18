ee20500fd4118247aa08fb27abe7a44fa0904aa3
Zig Zichterman <ziggr@ziggr.com>
2017-02-17 20:28:08 -0800

    Attempt delayed initialization of MM integration
    So that MM has a chance to fully initialize before we start querying it for start/end timestamps.

--- GuildSalesQuota.lua
+++ GuildSalesQuota.lua
@@ -39,7 +39,7 @@ GuildSalesQuota.user_records = {}
                         -- distrusting "nah, no more history"
 GuildSalesQuota.retry_ct   = { 0, 0, 0, 0, 0 }
 GuildSalesQuota.max_retry_ct = 3
-                        -- Filled in by FetchMMDateRanges()
+                        -- Filled in partially by DateRanges(), fully by FetchMMDateRanges()
 GuildSalesQuota.mm_date_ranges = nil
 
 -- UserGuildTotals -----------------------------------------------------------
@@ -355,7 +355,7 @@ function GuildSalesQuota:CreateSettingsWindow()
         , choices = {}
         , choicesValues = {}
         }
-    for index, mmdr in ipairs(GuildSalesQuota.FetchMMDateRanges()) do
+    for index, mmdr in ipairs(GuildSalesQuota.DateRanges()) do
         r.choices[index] = mmdr.name
         r.choicesValues[index] = index
     end
@@ -366,26 +366,53 @@ function GuildSalesQuota:CreateSettingsWindow()
             , self.OnPanelControlsCreated)
 end
 
--- Lazy fetch M.M. date ranges.
-function GuildSalesQuota.FetchMMDateRanges()
+-- Return a partially initialized date range table.
+-- Names are there, but start/end timestamps not yet: those don't get
+-- filled in until SaveNow() calls FetchMMDateRanges().
+function GuildSalesQuota.DateRanges()
     if GuildSalesQuota.mm_date_ranges then
         return GuildSalesQuota.mm_date_ranges
     end
 
-    local mmg = MMGuild:new("_not_really_a_guild")
     local r   = {}
-    r[1] = { name = "Today",        start_ts = mmg.oneStart,   end_ts = nil}
-    r[2] = { name = "Yesterday",    start_ts = mmg.twoStart,   end_ts = mmg.oneStart}
-    r[3] = { name = "This Week",    start_ts = mmg.threeStart, end_ts = nil}
-    r[4] = { name = "Last Week",    start_ts = mmg.fourStart,  end_ts = mmg.fourEnd}
-    r[5] = { name = "Prior Week",   start_ts = mmg.fiveStart,  end_ts = mmg.fiveEnd}
-    r[6] = { name = "Last 10 Days", start_ts = mmg.sixStart,   end_ts = nil}
-    r[7] = { name = "Last 30 Days", start_ts = mmg.sevenStart, end_ts = nil}
-    r[8] = { name = "Last 7 Days",  start_ts = mmg.eightStart, end_ts = nil}
+    r[1] = { name = "Today"        }
+    r[2] = { name = "Yesterday"    }
+    r[3] = { name = "This Week"    }
+    r[4] = { name = "Last Week"    }
+    r[5] = { name = "Prior Week"   }
+    r[6] = { name = "Last 10 Days" }
+    r[7] = { name = "Last 30 Days" }
+    r[8] = { name = "Last 7 Days"  }
+    r[9] = { name = "All History" }
+    GuildSalesQuota.mm_date_ranges = r
+    return GuildSalesQuota.mm_date_ranges
+end
+
+-- Lazy fetch M.M. date ranges.
+function GuildSalesQuota.FetchMMDateRanges()
+    local mmg = MMGuild:new("_not_really_a_guild")
+    local r   = GuildSalesQuota.DateRanges()
+    r[1].start_ts = mmg.oneStart        -- Today
+    r[1].end_ts   = nil
+    r[2].start_ts = mmg.twoStart        -- Yesterday
+    r[2].end_ts   = mmg.oneStart
+    r[3].start_ts = mmg.threeStart      -- This Week
+    r[3].end_ts   = nil
+    r[4].start_ts = mmg.fourStart       -- Last Week
+    r[4].end_ts   = mmg.fourEnd
+    r[5].start_ts = mmg.fiveStart       -- Prior Week
+    r[5].end_ts   = mmg.fiveEnd
+    r[6].start_ts = mmg.sixStart        -- Last 10 Days
+    r[6].end_ts   = nil
+    r[7].start_ts = mmg.sevenStart      -- Last 30 Days
+    r[7].end_ts   = nil
+    r[8].start_ts = mmg.eightStart      -- Last 7 Days
+    r[8].end_ts   = nil
                         -- Replace MM's "custom" date range with "All".
                         -- (Not worth the effort: I would have to dynamically
                         -- reload each time I updated UI or ran a scan)
-    r[9] = { name = "All History",  start_ts = nil,            end_ts = nil}
+    r[9].start_ts = nil
+    r[9].end_ts   = nil
     GuildSalesQuota.mm_date_ranges = r
     return GuildSalesQuota.mm_date_ranges
 end
