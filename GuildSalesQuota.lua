68c3be5bc5b12cdf093277e3a46c66f83e9ca624
Zig Zichterman <ziggr@ziggr.com>
2017-02-18 16:16:34 -0800

    Doc for 2.7.2

--- GuildSalesQuota.lua
+++ GuildSalesQuota.lua
@@ -2,7 +2,7 @@ local LAM2 = LibStub("LibAddonMenu-2.0")
 
 local GuildSalesQuota = {}
 GuildSalesQuota.name            = "GuildSalesQuota"
-GuildSalesQuota.version         = "2.7.1"
+GuildSalesQuota.version         = "2.7.2"
 GuildSalesQuota.savedVarVersion = 5
 GuildSalesQuota.default = {
       enable_guild  = { true, true, true, true, true }
